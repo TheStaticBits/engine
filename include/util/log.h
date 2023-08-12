06be293cf441fefd21f0df31c3ec6b59a3996657
TheStaticBits <puppy2655@gmail.com>
2023-08-11 22:10:48 -0400

    logger colors!

--- include/util/log.h
+++ include/util/log.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <unordered_map>
 
 namespace logger {
-    void output(const std::string& message);
+    extern const std::unordered_map<std::string, std::string> colors;
+
+    void output(const std::string& message, const std::string& prefix="");
+    void outputGreen(const std::string& message);
+    void outputYellow(const std::string& message);
+    void outputRed(const std::string& message);
 
     void info(const std::string& message);
     void warn(const std::string& message);
--- src/util/log.cpp
+++ src/util/log.cpp
@@ -4,17 +4,43 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-void logger::output(const std::string& message)
+#include <iostream>
+#include <unordered_map>
+
+const std::unordered_map<std::string, std::string> logger::colors = {
+    { "green", "\033[32m" },
+    { "yellow", "\033[33m" },
+    { "red", "\033[31m" }
+};
+
+void logger::output(const std::string& message, const std::string& prefix)
 {
 #ifdef DEBUG_MODE
+#ifdef __EMSCRIPTEN__
     std::cout << message << std::endl;
+#else
+    std::cout << prefix << message << "\033[0m" << std::endl;
+#endif
 #endif
 }
 
-void logger::info(const std::string& message) { logger::output("[INFO] " + message); }
-void logger::warn(const std::string& message) { logger::output("[WARN] " + message); }
-void logger::error(const std::string& message) { logger::output("[ERROR] " + message); }
-void logger::errorThrow(const std::string& message) { logger::output("[ERROR] " + message); throw std::runtime_error(message); }
-void logger::SDLError(const std::string& message) { logger::output("[SDL ERROR] " + message + " | " + SDL_GetError()); }
-void logger::IMGError(const std::string& message) { logger::output("[IMG ERROR] " + message + " | " + IMG_GetError()); }
-void logger::TTFError(const std::string& message) { logger::output("[TTF ERROR] " + message + " | " + TTF_GetError()); }
+void logger::info(const std::string& message)  { logger::output("[INFO] " + message); }
+void logger::warn(const std::string& message)  { logger::output("[WARN] " + message, logger::colors.at("yellow")); }
+void logger::error(const std::string& message) { logger::output("[ERROR] " + message, logger::colors.at("red")); }
+
+void logger::errorThrow(const std::string& message) { 
+    logger::output("[ERROR] " + message, logger::colors.at("red")); 
+    throw std::runtime_error(message); 
+}
+
+void logger::SDLError(const std::string& message) { 
+    logger::output("[SDL ERROR] " + message + " | " + SDL_GetError(), logger::colors.at("red")); 
+}
+
+void logger::IMGError(const std::string& message) { 
+    logger::output("[IMG ERROR] " + message + " | " + IMG_GetError(), logger::colors.at("red")); 
+}
+
+void logger::TTFError(const std::string& message) {
+    logger::output("[TTF ERROR] " + message + " | " + TTF_GetError(), logger::colors.at("red")); 
+}
